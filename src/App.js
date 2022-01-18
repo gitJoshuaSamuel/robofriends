138610ee18d3f16290ed55c0cc9ca9485d5d7194
gitJoshuaSamuel <joshuasamuel1997@gmail.com>
2022-01-18 14:34:00 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {robots} from './robots';
+import { CardList } from './CardList';
+import {SearchBox} from './SearchBox';
+import {Scroll} from './Scroll';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const state={
+   
+}
+
+class App extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            robots:[],
+            searchfield:''
+        }//things that can change and affect our app
+    }
+
+    componentDidMount(){
+        //this.setState({robots:robots})
+        fetch('https://jsonplaceholder.typicode.com/users').then(response=>response.json())
+        .then(users=>{
+            this.setState({robots:users})
+        })
+    }
+
+    onSearchChange=(event)=>{
+        this.setState({searchfield:event.target.value})
+        
+        // console.log(filteredRobots);
+    }
+
+    render(){
+
+        const filteredRobots=this.state.robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        });
+
+        if(this.state.robots.length===0){
+            return <h1>loading</h1>
+        }
+        else{
+            return (
+                <div className='tc'> 
+                    <h1>Robo Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                   
+                </div>
+            ); 
+        }
+
+        
+    }
+   
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; //connects react package to the DOM
 import './index.css';
 import App from './App';
+import CardList from './CardList';
+import 'tachyons';
+//import registerServiceWorker from './registerServiceWorker';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
 reportWebVitals();
+//registerServiceWorker();
